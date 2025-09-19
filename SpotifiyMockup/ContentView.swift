a582baf0f5971ce8a9cfcabe42a06dfd95c119f3
Dylan Chhum <dylchhum@berkeley.edu>
2025-09-19 12:28:44 -0700

    Initial Commit

---
+++ SpotifiyMockup/ContentView.swift
@@ -0,0 +1,106 @@
+//
+//  ContentView.swift
+//  discussion3
+//
+//  Created by Dylan Chhum on 7/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    var body: some View {
+        ZStack {
+            albumView()
+                
+            
+            BlurView(style: .systemMaterialDark)
+                .ignoresSafeArea(.all)
+//           
+            VStack {
+                extraView()
+                albumView()
+                titleView()
+                timeView()
+                playbackView()
+            }
+        }
+    }
+     
+}
+
+// Do not worry about this
+struct BlurView: UIViewRepresentable {
+    var style: UIBlurEffect.Style
+
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        let view = UIVisualEffectView(effect: UIBlurEffect(style: style))
+        return view
+    }
+
+    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+        uiView.effect = UIBlurEffect(style: style)
+    }
+}
+
+
+struct titleView: View {
+    var body: some View {
+        Text("Implement Me")
+    }
+}
+
+struct timeView: View {
+    var body: some View {
+        VStack {
+            ZStack {
+                Circle()
+                    .frame(height:10)
+                    .foregroundColor(.white)
+                    .padding([.trailing], 370)
+                Rectangle()
+                    .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
+                    .frame(height: 5)
+                    .padding([.leading, .trailing], 10)
+                    .foregroundColor(.white)
+                    .opacity(0.3)
+            }
+            
+            HStack {
+                Text("0:00")
+                    .opacity(0.6)
+                    .foregroundColor(.white)
+                Spacer()
+                Text("-2:53")
+                    .opacity(0.6)
+                    .foregroundColor(.white)
+            }
+            .padding([.trailing, .leading], 10)
+        }
+    }
+}
+
+struct playbackView: View {
+    var body: some View {
+        Text("Implement Me")
+    }
+}
+
+struct albumView : View {
+    var body: some View {
+        Image("Image")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding(10)
+    }
+}
+
+struct extraView : View {
+    var body: some View {
+        Text("Implement Me")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpotifiyMockup/SpotifiyMockupApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifiyMockupApp.swift
+//  SpotifiyMockup
+//
+//  Created by Dylan Chhum on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct SpotifiyMockupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
